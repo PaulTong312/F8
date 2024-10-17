bf42c19253c9d0e4616d303a6495a4266c84e9f7
Tong Duy Khang <tongduykhang01012004@gmail.com>
2024-10-17 11:16:35 +0700

    feature: add course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -10,6 +10,20 @@ class CourseController {
             )
             .catch(next);
     }
+
+    create(req, res, next) {
+        res.render('course/create');
+    }
+
+    store(req, res, next) {
+        let formData = req.body;
+        formData.image = `https://i.ytimg.com/vi/${req.body.videoId}/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLD9QFkahguHM2_ISOBWeVW1UwbaMw`;
+        const course = new Course(formData);
+        course
+            .save()
+            .then(() => res.redirect('/'))
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController();
